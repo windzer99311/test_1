4ec7f50bfe05b45cf3f21d82f1423f3615195375
windzer99311 <windzergamer@gmail.com>
2026-01-15 02:28:37 +0530

    Update install_ffmpeg.sh

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "Installing FFmpeg..."
+
 mkdir -p api/bin
 cd api/bin
 
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+curl -sL https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+
 tar -xf ffmpeg.tar.xz
-mv ffmpeg-*-amd64-static/ffmpeg ./ffmpeg
+cp ffmpeg-*-amd64-static/ffmpeg ./ffmpeg
 chmod +x ffmpeg
 
 rm -rf ffmpeg-*-amd64-static ffmpeg.tar.xz
+
+echo "FFmpeg installed"
